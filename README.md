69a5994421e48864f6f4bbcf3d00c0c38c3d8431
Joseph M. Stujenske, MD, PhD <jmstujenske@gmail.com>
2022-06-01 15:35:58 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Spatiotemporal Dynamics Fiber Optic
- Scripts from Stujenske et al., Cell Reports, 2014 for modeling light and heat spread for a fiber optic in the brain
+ Scripts from Stujenske et al., Cell Reports, 2015 for modeling light and heat spread for a fiber optic in the brain
+
+If you use this, please cite the originating paper:
+Stujenske JM, Spellman T, Gordon JA. (2015). Modeling the Spatiotemporal Dynamics of Light and Heat Propagation for In Vivo Optogenetics. Cell Reports 12, p525-534.
+
+https://www.cell.com/cell-reports/fulltext/S2211-1247(15)00648-8
---
+++ Scripts/LightHeat_example.m
@@ -0,0 +1,18 @@
+%Example usage of Light and Heat functions
+%Generate 3 plots for 532 nm light:
+%Figure 1 - Intensity relative to maximum intensity (log scale) from a 62
+%nm diameter, .22 NA fiber
+%Figure 2 - Steady state temperature change for the same fiber
+%Figure 3 - Time course of temperature change for the same fiber during and
+%after 60 seconds of illumination
+%
+%Written by Joseph M. Stujenske
+%Stujenske et al., Cell Reports, 2014
+%
+%Anticipate this to take >30 minutes to compute.
+
+[frac_abs,frac_trans,params,r,depth]=MonteCarloLight(1,.031,.22,532,100);
+figure; LightHeatPlotter(r,depth,log10(frac_trans./max(frac_trans(:))),532);caxis([-4 0]);
+[u_time,u_space,t,r,depth]=HeatDiffusionLight(frac_abs,params,.03,0,60,120,10,6,.25);
+figure; LightHeatPlotter(r,depth,u_space,'hot');caxis([0 4.2])
+figure; imagesc(t,depth,u_time); colormap('hot');caxis([0 2.2])
